af81376abf25b78b421897b0e18cb33f43b24aad
Tanawat Jukmongkol <tanawat.jukmon@gmail.com>
2024-09-01 16:20:29 +0700

    hypridle now works

--- home-manager/src/hyprland.nix
+++ home-manager/src/hyprland.nix
@@ -241,7 +241,18 @@
 			];
 		};
 	};
-	#programs.hypridle = {
-	#	enable = true;
-	#};
+	services.hypridle = {
+		enable = true;
+		settings = {
+			general = {
+				lock_cmd = "pidof hyprlock || hyprlock";
+				before_sleep_cmd = "loginctl lock-session";
+				after_sleep_cmd = "hyprctl dispatch dpms on";
+			};
+			listener = {
+				timeout = "80";
+				on-timeout = "loginctl lock-session";
+			};
+		};
+	};
 }
