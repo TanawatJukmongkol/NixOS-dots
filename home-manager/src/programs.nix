8875857816b5c35ee4fb897a7cfd85c0ac0e7e4b
Tanawat Jukmongkol <tanawat.jukmon@gmail.com>
2024-09-01 15:58:03 +0700

    nvim autocomplete

--- home-manager/src/programs.nix
+++ home-manager/src/programs.nix
@@ -35,7 +35,11 @@
 			map kitty_mod+enter		new_window_with_cwd
 		'';
 	};
-	programs.neovim = {
+	programs.neovim =
+	let
+		toLua = str: "lua << EOF\n${str}\nEOF\n";
+	in
+	{
 		enable = true;
 		defaultEditor = true;
 		viAlias = true;
@@ -43,30 +47,100 @@
 		vimdiffAlias = true;
 		plugins = with pkgs.vimPlugins; [
 			transparent-nvim
-			vim-airline
-			vim-airline-themes
-			nvim-treesitter
+			vim-airline {
+				plugin = vim-airline-themes;
+				config = ''
+					let g:airline_powerline_fonts = 1
+					let g:airline_theme = 'owo'
+				'';
+			}
+			{
+				plugin = comment-nvim;
+				config = toLua ''
+					require("Comment").setup {}
+				'';
+			}
+			nvim-lspconfig mason-nvim {
+				plugin = mason-lspconfig-nvim;
+				config = toLua ''
+					require("mason").setup()
+					require("mason-lspconfig").setup {
+						ensure_installed = { "nil_ls", "clangd" }
+					}
+					require("lspconfig").nil_ls.setup {}
+					require("lspconfig").clangd.setup {}
+				'';
+			}
+			cmp-nvim-lsp cmp-buffer cmp-path cmp-cmdline {
+				plugin = nvim-cmp;
+				config = toLua ''
+					local cmp = require('cmp');
+					cmp.setup {
+						mapping = cmp.mapping.preset.insert({
+							['<C-b>'] = cmp.mapping.scroll_docs(-4),
+							['<C-f>'] = cmp.mapping.scroll_docs(4),
+							['<C-Space>'] = cmp.mapping.complete(),
+							['<C-e>'] = cmp.mapping.abort(),
+							['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+						}),
+						sources = {
+							{ name = 'nvim_lsp' },
+							{ name = 'buffer' },
+						}
+					}
+					cmp.setup.cmdline({ '/', '?' }, {
+						mapping = cmp.mapping.preset.cmdline(),
+							sources = {
+								{ name = 'buffer' }
+							}
+						})
+					cmp.setup.cmdline(':', {
+						mapping = cmp.mapping.preset.cmdline(),
+						sources = cmp.config.sources({
+							{ name = 'path' }
+						}, {
+							{ name = 'cmdline' }
+						}),
+						matching = { disallow_symbol_nonprefix_matching = false }
+					})
+				'';
+			}
+			{
+				plugin = (nvim-treesitter.withPlugins (p: [
+					p.tree-sitter-nix
+					p.tree-sitter-vim
+					p.tree-sitter-bash
+					p.tree-sitter-lua
+					p.tree-sitter-python
+					p.tree-sitter-json
+					p.tree-sitter-c
+				]));
+				config = toLua ''
+					require('nvim-treesitter.configs').setup {
+						ensure_installed = {},
+						auto_install = false,
+						highlight = { enable = true },
+						indent = { enable = true },
+					}
+				'';
+			}
 		];
 		extraConfig = ''
-			" set					autoindent
-			set						smartindent
-			set						noexpandtab
-			" set					expandtab
-			set						tabstop=4
-			set						shiftwidth=4
-			set						backspace=indent,eol,start
-			syntax					on
-			set						nu
-			set						list
-			set						listchars+=space:⋅
-			set						listchars+=tab:→\ 
-			set						listchars+=eol:↴
-
+			" set		autoindent
+			set			smartindent
+			set			noexpandtab
+			" set		expandtab
+			set			tabstop=4
+			set			shiftwidth=4
+			set			backspace=indent,eol,start
+			" syntax	on
+			set			nu
+			set			list
+			set			listchars+=space:⋅
+			set			listchars+=tab:→\ 
+			set			listchars+=eol:↴
 			hi Pmenu		ctermfg=white ctermbg=black gui=NONE guifg=white guibg=black
 			hi PmenuSel		ctermfg=white ctermbg=blue gui=bold guifg=white guibg=purple
-
-			let g:airline_powerline_fonts = 1
-			let g:airline_theme='owo'
 		'';
 	};
 	programs.fastfetch = {
--- home-manager/src/stylix-home.nix
+++ home-manager/src/stylix-home.nix
@@ -3,4 +3,5 @@
   ...
 } : {
   stylix.targets.kitty.variant256Colors = true;
+  stylix.targets.neovim.enable = false;
 }
